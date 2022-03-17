285aa7c80357e0d24b5bed9a73cb641c41098f97
csaba19999 <menesi777@gmail.com>
2022-03-17 16:04:50 +0100

    kereso backend szűrési feltételek alapjai.

--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('auto_fill', AutokListazasaController::class);
 Route::apiResource('kedvezmeny', KedvezmenyekController::class);
-
-Route::get('auto_fill', [AutokListazasaController::class, 'index']);
+Route::get('auto_fill/{mezo}/{helyszin}/{elvitel}/{visszahoz}/{marka}/{modell}/{kivitel}/{uzemanyag}/{evTol}/{evIg}/{arTol}/{arIg}', [AutokListazasaController::class, 'keresesParameteresen']);
 Route::get('kedvezmeny', [KedvezmenyekController::class, 'kedvezmenyek']);
-//Route::delete('adminAutok/{alvazSzam}', [AutokListazasaController::class, 'destroy']); 
+//Route::delete('adminAutok/{alvazSzam}', [AutokListazasaController::class, 'destroy']);
+
+
 
 Route::apiResource('keresoview', KeresoViewController::class);
 Route::get('keresoview', [KeresoViewController::class, 'index']);
