e8e4c72d5fa60ffc646319f2c6532b5d81463353
lejlailles <il970322@gmail.com>
2022-03-21 11:43:23 +0100

    felhasználóadmin törlés+felhasználó select szűrés

--- public/js/admin/jsFelhasznalok.js
+++ public/js/admin/jsFelhasznalok.js
@@ -4,15 +4,43 @@ $(function () {
     const felhasznalok = [];
     let url = "http://localhost:8000/";
 
-    let apiVegpont = url + "api" + "/" + "felhasznalo";
+    let apiVegpont = url + "api/felhasznaloadmin";
 
     const szuloElem = $(".felhasznalokAdmin");
     const sablonElem = $(".felhasznalo");
     szuloElem.empty();
+    myAjax.getAdat(apiVegpont, felhasznalok, Megjelenit1);
 
-    myAjax.getAdat(apiVegpont, felhasznalok, Megjelenit);
+    Jogkorokszerint();
 
-    function Megjelenit() {
+    $(window).on("torol", (event) => {
+        const id = event.detail.id;
+
+        myAjax.deleteAdat(apiVegpont, id);
+
+     
+    });
+
+    function Jogkorokszerint() {
+        $("#jogkorKategoriak").on("change", function () {
+            if ($("#jogkorKategoriak").val() === "osszes") {
+                szuloElem.empty();
+
+                myAjax.getAdat(apiVegpont, felhasznalok, Megjelenit1);
+            }
+
+            if ($("#jogkorKategoriak").val() === "felhasznaloJog") {
+                szuloElem.empty();
+                myAjax.getAdat(apiVegpont, felhasznalok, Megjelenit2);
+            }
+            if ($("#jogkorKategoriak").val() === "adminJog") {
+                szuloElem.empty();
+                myAjax.getAdat(apiVegpont, felhasznalok, Megjelenit3);
+            }
+        });
+    }
+
+    function Megjelenit1() {
         szuloElem.show();
         $(".felhasznalokAdmin").append("<div class='felhFejlec'></div>");
         $(".felhFejlec").append(
@@ -20,15 +48,43 @@ $(function () {
         );
 
         felhasznalok.forEach(function (elem) {
-            
-
             const ujElem = sablonElem.clone().appendTo(szuloElem);
             const ujTermek = new Felhasznalo(ujElem, elem);
         });
 
         ModositMegjelenes();
+    }
 
-        sablonElem.hide();
+    function Megjelenit2() {
+        szuloElem.show();
+        $(".felhasznalokAdmin").append("<div class='felhFejlec'></div>");
+        $(".felhFejlec").append(
+            "<h2>Jogkör</h2><h2>Felhasználónev</h2><h2>E-mail</h2><h2>Reg. dátum</h2><h2></h2><h2></h2><h2></h2>"
+        );
+
+        felhasznalok.forEach(function (elem) {
+            if (elem["jogkor"] === 1) {
+                const ujElem = sablonElem.clone().appendTo(szuloElem);
+                const ujTermek = new Felhasznalo(ujElem, elem);
+            }
+        });
+        ModositMegjelenes();
+    }
+
+    function Megjelenit3() {
+        szuloElem.show();
+        $(".felhasznalokAdmin").append("<div class='felhFejlec'></div>");
+        $(".felhFejlec").append(
+            "<h2>Jogkör</h2><h2>Felhasználónev</h2><h2>E-mail</h2><h2>Reg. dátum</h2><h2></h2><h2></h2><h2></h2>"
+        );
+
+        felhasznalok.forEach(function (elem) {
+            if (elem["jogkor"] === 2) {
+                const ujElem = sablonElem.clone().appendTo(szuloElem);
+                const ujTermek = new Felhasznalo(ujElem, elem);
+            }
+        });
+        ModositMegjelenes();
     }
 
     function ModositMegjelenes() {
